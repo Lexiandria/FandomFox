9152873ab3e5d5e873ad68dc4e1cc99aa75d525e
NewMoonGamer <newmoongamer@gmail.com>
2017-07-14 13:58:54 -0400

    use the blog for testing ^.^

---
+++ FandomFoxForum/forumenv/Scripts/misago-start.py
@@ -0,0 +1,6 @@
+#!c:\users\newmoongamer\desktop\fandomfox\fandomfoxforum\forumenv\scripts\python.exe
+from misago.core.setup import start_misago_project
+
+
+if __name__ == "__main__":
+    start_misago_project()
