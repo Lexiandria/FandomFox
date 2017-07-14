d0227f2345a1de0ec2b7b2c6d6294fda0611c1d8
NewMoonGamer <newmoongamer@gmail.com>
2017-07-14 11:17:29 -0400

    saving in case I have to delete the entire folder again.

--- FandomFoxForum/myvenv/Scripts/misago-start.py
+++
@@ -1,6 +0,0 @@
-#!C:\Users\Girls Who Code\Desktop\FandomFox\FandomFoxForum\myvenv\Scripts\python.exe
-from misago.core.setup import start_misago_project
-
-
-if __name__ == "__main__":
-    start_misago_project()
